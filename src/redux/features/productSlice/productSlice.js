68cd90e3439f7501e2e66dd891da9bd9758e944c
sabbir53rahman <sabbir53rahman@gmail.com>
2025-07-16 11:31:05 +0600

    sdg

--- src/redux/features/productSlice/productSlice.js
+++ src/redux/features/productSlice/productSlice.js
@@ -1,146 +1,154 @@
 import { apiSlice } from "../apiSlice/apiSlice";
 
 export const productApi = apiSlice.injectEndpoints({
-    overrideExisting: true,
-    endpoints : (builder) =>({
-        addProduct : builder.mutation({
-            query: (info) => {
-                return {
-                    url: "/products",
-                    method: "POST",
-                    body: info,
-                };
-            },
-            invalidatesTags : ["Product"]
-        }),
-        getPaginatedProducts : builder.query({
-            query : (query)=>{
-                return {
-                    url : `/products/paginated?page=${query}`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        getAllProducts : builder.query({
-            query : ()=>{
-                return {
-                    url : `/products`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        getProduct : builder.query({
-            query : (query)=>{
-                return {
-                    url : `/products/${query}`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        updateProduct : builder.mutation({
-            query : (info)=>{
-                return {
-                    url : `/products/${info?.id}`,
-                    method : "PATCH",
-                    body : info?.updatedData
-                };
-            },
-            invalidatesTags : ['Product']
-        }),
-        deleteProduct : builder.mutation({
-            query : (info)=>{
-                return {
-                    url : `/products/${info}`,
-                    method : "DELETE",
-                };
-            },
-            invalidatesTags : ['Product']
-        }),
-        addCategory : builder.mutation({
-            query: (info) => {
-                return {
-                    url: "/categorys",
-                    method: "POST",
-                    body: info,
-                };
-            },
-            invalidatesTags : ["Category"]
-        }),
-        getAllCategorys : builder.query({
-            query : ()=>{
-                return {
-                    url : `/categorys`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        getLowStock : builder.query({
-            query : ()=>{
-                return {
-                    url : `/products/lowStock`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        topSelling10 : builder.query({
-            query : ()=>{
-                return {
-                    url : `/products/topSelling`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        updateIsFeatured : builder.mutation({
-            query : ({isFeature,productId})=>{
-                return {
-                    url : `/products/${productId}`,
-                    method : "PATCH",
-                    body : {isFeatured : isFeature}
-                };
-            },
-            invalidatesTags : ['Product']
-        }),
-        getFeaturedProducts : builder.query({
-            query : ()=>{
-                return {
-                    url : `/products/getFeatured`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        getLeatestProducts : builder.query({
-            query : ()=>{
-                return {
-                    url : `/products/leatest-product`
-                };
-            },
-            providesTags : ['Product']
-        }),
-        getProductsByCategory : builder.query({
-            query : (query)=>{
-                return {
-                    url : `/products/by-category/${query}`
-                };
-            },
-            providesTags : ['Product']
-        }),
-
-    })
-})
+  overrideExisting: true,
+  endpoints: (builder) => ({
+    addProduct: builder.mutation({
+      query: (info) => {
+        return {
+          url: "/products",
+          method: "POST",
+          body: info,
+        };
+      },
+      invalidatesTags: ["Product"],
+    }),
+    getPaginatedProducts: builder.query({
+      query: (query) => {
+        return {
+          url: `/products/paginated?page=${query}`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    getAllProducts: builder.query({
+      query: () => {
+        return {
+          url: `/products`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    getProduct: builder.query({
+      query: (query) => {
+        return {
+          url: `/products/${query}`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    updateProduct: builder.mutation({
+      query: (info) => {
+        return {
+          url: `/products/${info?.id}`,
+          method: "PATCH",
+          body: info?.updatedData,
+        };
+      },
+      invalidatesTags: ["Product"],
+    }),
+    deleteProduct: builder.mutation({
+      query: (info) => {
+        return {
+          url: `/products/${info}`,
+          method: "DELETE",
+        };
+      },
+      invalidatesTags: ["Product"],
+    }),
+    addCategory: builder.mutation({
+      query: (info) => {
+        return {
+          url: "/categorys",
+          method: "POST",
+          body: info,
+        };
+      },
+      invalidatesTags: ["Category"],
+    }),
+    getAllCategorys: builder.query({
+      query: () => {
+        return {
+          url: `/categorys`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    getLowStock: builder.query({
+      query: () => {
+        return {
+          url: `/products/lowStock`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    topSelling10: builder.query({
+      query: () => {
+        return {
+          url: `/products/topSelling`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    updateIsFeatured: builder.mutation({
+      query: ({ isFeature, productId }) => {
+        return {
+          url: `/products/${productId}`,
+          method: "PATCH",
+          body: { isFeatured: isFeature },
+        };
+      },
+      invalidatesTags: ["Product"],
+    }),
+    getFeaturedProducts: builder.query({
+      query: () => {
+        return {
+          url: `/products/getFeatured`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    getLeatestProducts: builder.query({
+      query: () => {
+        return {
+          url: `/products/leatest-product`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    getProductsByCategory: builder.query({
+      query: (query) => {
+        return {
+          url: `/products/by-category/${query}`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+    getProductsByPriceRange: builder.query({
+      query: ({ min, max }) => {
+        return {
+          url: `/products/price-range?min=${min}&max=${max}`,
+        };
+      },
+      providesTags: ["Product"],
+    }),
+  }),
+});
 
 export const {
-    useAddProductMutation,
-    useGetPaginatedProductsQuery,
-    useGetAllProductsQuery,
-    useGetProductQuery,
-    useUpdateProductMutation,
-    useDeleteProductMutation,
-    useAddCategoryMutation,
-    useGetAllCategorysQuery,
-    useGetLowStockQuery,
-    useTopSelling10Query,
-    useUpdateIsFeaturedMutation,
-    useGetFeaturedProductsQuery,
-    useGetLeatestProductsQuery,
-    useGetProductsByCategoryQuery,
-} = productApi
+  useAddProductMutation,
+  useGetPaginatedProductsQuery,
+  useGetAllProductsQuery,
+  useGetProductQuery,
+  useUpdateProductMutation,
+  useDeleteProductMutation,
+  useAddCategoryMutation,
+  useGetAllCategorysQuery,
+  useGetLowStockQuery,
+  useTopSelling10Query,
+  useUpdateIsFeaturedMutation,
+  useGetFeaturedProductsQuery,
+  useGetLeatestProductsQuery,
+  useGetProductsByCategoryQuery,
+  useGetProductsByPriceRangeQuery,
+} = productApi;
