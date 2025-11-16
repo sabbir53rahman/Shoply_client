720fcd9f8a25f3faf632aed53e004caac23d258d
tahsin-dev225 <ahmedtasin225@gmail.com>
2025-11-16 21:12:40 +0600

    footer

--- src/components/Footer/FooterMain.jsx
+++ src/components/Footer/FooterMain.jsx
@@ -30,19 +30,6 @@ const FooterMain = () => (
                 <p className="text-sm text-gray-600">Send us an email</p>
               </div>
             </div>
-
-            
-
-            <div className="flex items-center gap-3">
-              <div className="w-10 h-10 bg-orange-100 rounded-lg flex items-center justify-center">
-                <Clock className="w-5 h-5 text-orange-600" />
-              </div>
-              <div>
-                <p className="text-gray-900 font-medium">Store Hours</p>
-                <p className="text-sm text-gray-600">Mon-Fri: 9:00 to 5:00</p>
-                <p className="text-sm text-gray-600">Saturday: 9:00 to 3:00</p>
-              </div>
-            </div>
           </div>
         </div>
 
